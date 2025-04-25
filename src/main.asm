dce6e4d1ea1c4273bab1cb3705d97763794ccdea
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-04-24 20:04:49 -0700

    ROM Entry Point
    This stores all the ROM data properly so that if we use an emulator with BIOS, it triggers the Nintendo animation

--- src/main.asm
+++ src/main.asm
@@ -2,6 +2,24 @@ INCLUDE "hardware.inc"
 
 SECTION "Header", ROM0[$100]
     jp EntryPoint
+
+    ;This generates the Nintendo logo. It's a macro we can call to
+    NINTENDO_LOGO
+
+    ;We use 0x0100 through 0x014F to store ROM data, this is said data
+    DB "SNAKEBOY"
+    DS 11-10, 0
+    DB CART_COMPATIBLE_DMG_GBC
+    DB "HB"
+    DB CART_INDICATOR_GB
+    DB CART_ROM_32KB
+    DB CART_SRAM_NONE
+    DB CART_DEST_NON_JAPANESE
+    DB $33
+    DB 0
+    DB 0
+    DW 0
+    
     ds $150 - @, 0
 
 SECTION "Entry Point", ROM0[$150]
